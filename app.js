54878365715b0661eeb59626049ae45fa1e63243
= <eduardozwenger@hotmail.com>
2023-04-21 17:58:36 -0300

    Probando conexion a mongo

---
+++ app.js
@@ -0,0 +1,29 @@
+const { mongo } = require("./configuracion/mongo");
+const express = require("express")
+const cors = require("cors")
+
+
+// iniciar app
+console.log("App de node arrancada");
+
+
+// Conectar a base de datos
+connection();
+
+// Crear servidor de node
+const app = express();
+//const puerto = 3900;
+
+// Configurar cors
+app.use(cors());
+
+// convertir body a objeto js
+app.use(express.json()); // recibir datos con content- type app/json
+app.use(express.urlencoded({extended:true})); // form-url-encoer
+
+
+
+// Crear servidores y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("servidor corriendo en el puerto"+ " " +puerto);
+});
---
+++ configuracion/mongo.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+
+
+const connection = async() => {
+
+    try{
+        await mongoose.connect("mongodb://127.0.0.1:27017/mi_redsocial");
+
+        console.log("Conectado correctamente a bd: mi_redsocial")
+
+    }catch(error){
+        console.log(error);
+        throw new Error("No se ha podido conectar a la base de datos!!")
+    }
+
+
+}
+
+
+
+module.exports= connection
+
+
+
+
+//"mongodb+srv://eduardozwenger:wikideas@cluster0.9q1docc.mongodb.net/test"
