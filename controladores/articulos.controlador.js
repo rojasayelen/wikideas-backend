e518a62a9b95a8e6170f3304d4bfd6b7ed5c1691
rojasyelen <aye.estudio@gmail.com>
2023-05-30 19:02:22 -0300

    depurando codigo

--- controladores/articulos.controlador.js
+++ controladores/articulos.controlador.js
@@ -3,51 +3,38 @@ const fs = require('fs');
 const path = require('path');
 const Articulo = require('../modelos/articulos.modelo');
 
+
 //controlador de creacion de articulos
 const articuloPost = async (req, res) => {
+   
+   try {
     const params = req.body;
-    
-    // Validar params
-    try {
-        
-        let validar_titulo = !validator.isEmpty(params.titulo) &&
-                                validator.isLength(params.titulo, {min: 5, max: undefined});
-        let validar_contenido = !validator.isEmpty(params.contenido);
-
-        if (!validar_titulo || !validar_contenido) {
-                throw new Error('Invalid');
-        }    
-    }catch(err){
-        return res.status(400).send({
-            status: 'error',
-            message: 'Faltan datos por enviar'});
-    }
-    // crear el articulo segun los parametros requeridos por el Body
-    const articulo = new Articulo({params});
-
-   try{
-    // Guardar el articulo en la bbdd
+    const articulo = new Articulo({
+        titulo: params.titulo,
+        contenido: params.contenido,
+        imagen: params.imagen,
+        fecha: params.fecha
+    });
+   
     articulo.save().then((articuloStored) => {
         if(!articuloStored){
             res.status(404).send('no se ha podido realizar la petiticion');
         }else{
-            res.status(200).send({
-                status: 'success',
-                articulo:articuloStored,
-                mesaje: "Articulo en la bbdd"
-            });
+            res.status(200).send(articuloStored);
         }    
     });
-    } 
-    catch (error) {
-        res.status(500).send(error);   
+    } catch (error) {
+        res.status(500).send(error);  
     }
 }
 
-//controlador de consultas 
+
+//controlador de consultas
 const articuloGet = async (req, res = response) => {
+
 
     // res.send({message: 'probando get'});
 };
+
 
 module.exports = { articuloGet, articuloPost}
