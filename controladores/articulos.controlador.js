1fc87a64cbf0c41cb8b1a3d78ce5f3a784c940b6
= <eduardozwenger@hotmail.com>
2023-06-02 17:50:27 -0300

    Se modifico buscarget

--- controladores/articulos.controlador.js
+++ controladores/articulos.controlador.js
@@ -41,26 +41,44 @@ const articuloGet = async (req, res = response) => {
 }
 
 
-const buscarGet =  async (req, res) =>{
-  try{
-    let consultas;
-    if (req.query.q) {
+const buscarGet = async (req, res) => {
+  try {
+    console.log('Consulta de búsqueda:', req.params.palabra); // Verifica el valor del parámetro de búsqueda
+
+    let consultas = [];
+    if (req.params.palabra) {
       consultas = await Articulo.find(
-        {$text: {
-          $search: req.query.q
-        }},
         {
-          score: { $meta: 'textScore' } //Score clasifica los resultados desde el que mas se ajusta a su buscada hasta el que menos
+          $or:[ { contenido: { $regex: req.params.palabra, $options: 'i' } },
+                { titulo: { $regex: req.params.palabra, $options: 'i' } }],
+          $text: {
+            $search: req.params.palabra,
+          },
+        },
+        {
+          score: { $meta: 'textScore' } // Score clasifica los resultados desde el que más se ajusta a su búsqueda hasta el que menos
         }
-      ).sort({
-        score: { $meta: 'textScore' }
-      })
+      ).sort({fecha: -1});
     }
-    res.status(200).send(consultas);
-  }catch {
-    res.status(500).json({msg: 'error en la busqueda'});
+
+    console.log('Consulta MongoDB:', consultas); // Verifica el valor de las consultas de MongoDB
+
+    res.status(200).json(consultas);
+  } catch (error) {
+    res.status(500).json({ msg: 'Error en la búsqueda', error: error.message });
   }
-}
+};
+
+
+
+
+
+
+
+
+
+
+
 
 
 
